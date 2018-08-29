e64eb89b144261561a3a29a32c319e101d764e38
nadav <tashernadav@gmail.com>
2018-08-29 09:20:25 -0400

    V0.0 #: 01

---
+++ app/src/main/java/nadav/tasher/webchange/Home.java
@@ -0,0 +1,12 @@
+package nadav.tasher.webchange;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
